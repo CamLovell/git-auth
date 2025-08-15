c58638a7ce1d490ef1993bb3a08f616c7af51731
Cameron Lovell <cameron.e.lovell@gmail.com>
2025-08-15 16:40:45 +1000

    username retrieval added
    get command works and validates git properly but there is no persistent
    storage yet

--- src/lib.rs
+++ src/lib.rs
@@ -2,32 +2,63 @@ use serde_json::Value;
 use std::{
     collections::HashMap,
     fs,
-    io::{self, Write},
+    io::{self, Read, Write},
     thread,
     time::Duration,
 };
 
-pub fn send_creds() {
-    let pass_file = fs::read_to_string("pass.txt").unwrap();
-    let pass = pass_file.lines().next().unwrap(); // Only want the first line
-
-    let _ = io::stdout().write_all(b"username=CamLovell\n");
-    let _ = io::stdout().write_all(format!("password={}\n", pass).as_bytes());
-    let _ = io::stdout().write_all(b"quit=1\n");
+#[allow(dead_code)] // Complains because it isn't used anywhere in lib.rs
+#[derive(Debug)]
+pub struct GitRequest {
+    host: String,
+    protocol: String,
+    path: Option<String>,
 }
 
-pub fn get_oauth() {
+impl GitRequest {
+    pub fn from_stdin() -> Self {
+        let mut buf = String::new();
+        // TODO: Proper error handling!
+        let _ = io::stdin().read_to_string(&mut buf).unwrap();
+        let stdin_info: HashMap<String, String> = buf
+            .lines()
+            .map(|l| {
+                let (k, v) = l.split_once("=").unwrap();
+                (k.trim().to_string(), v.trim().to_string())
+            })
+            .collect();
+
+        GitRequest {
+            protocol: stdin_info.get("protocol").unwrap().to_string(),
+            host: stdin_info.get("host").unwrap().to_string(),
+            path: stdin_info.get("path").cloned(),
+        }
+    }
+}
+
+pub struct Creds {
+    username: String,
+    password: String,
+}
+
+pub fn send_creds(creds: &Creds) {
+    println!("username={}", creds.username);
+    println!("password={}", creds.password);
+    println!("quit=1");
+}
+
+pub fn get_oauth() -> Creds {
     let client_id = fs::read_to_string("id.txt").unwrap();
     let client = reqwest::blocking::Client::new();
-    let params = [("scope", "repo"), ("client_id", client_id.as_str())];
+    let params = [("scope", "repo read:user"), ("client_id", client_id.trim())];
     let response = client
         .post("https://github.com/login/device/code")
-        .header("Accept", "application/json")
+        .header("Accept", "application/vnd.github+json ")
         .form(&params)
         .send()
         .unwrap();
     let txt = response.text().unwrap();
-    eprintln!("{:?}", txt);
+    eprintln!("CODE: {:?}", txt);
     let data: Value = serde_json::from_str(&txt).unwrap();
 
     let device_code = data["device_code"].as_str().unwrap();
@@ -35,31 +66,38 @@ pub fn get_oauth() {
     eprintln!("{device_code}");
     let poll_params = [
         ("device_code", device_code),
-        ("client_id", client_id.as_str()),
+        ("client_id", client_id.trim()),
         ("grant_type", "urn:ietf:params:oauth:grant-type:device_code"),
     ];
     loop {
         let res = client
             .post("https://github.com/login/oauth/access_token")
-            .header("Accept", "application/json")
+            .header("Accept", "application/vnd.github+json")
             .form(&poll_params)
             .send()
             .unwrap();
         let txt = res.text().unwrap();
         let data: HashMap<String, Value> = serde_json::from_str(&txt).unwrap();
+        eprintln!("{:?}", data);
         if let Some(token) = data.get("access_token") {
             let mut file = fs::File::create("pass.txt").unwrap();
-            let _ = file.write_all(
-                token
-                    .to_string()
-                    .strip_prefix("\"")
-                    .unwrap()
-                    .strip_suffix("\"")
-                    .unwrap()
-                    .as_bytes(),
-            );
-            eprintln!("{}", token);
-            break;
+            let token_str = token.as_str().unwrap();
+            let _ = file.write_all(token_str.as_bytes());
+            let auth = format!("token {token_str}");
+            let res = client
+                .get("https://api.github.com/user")
+                .header("User-Agent", "git-auth")
+                .header("Accept", "application/vnd.github+json")
+                .header("Authorization", auth)
+                .send()
+                .unwrap();
+            let txt = res.text().unwrap();
+            let data: HashMap<String, Value> = serde_json::from_str(&txt).unwrap();
+            eprintln!("{:?}", data["login"].as_str());
+            return Creds {
+                username: data["login"].as_str().unwrap().to_string(),
+                password: token_str.to_string(),
+            };
         }
         thread::sleep(Duration::from_secs(5));
     }
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,70 @@
-use std::io::{self, Read, Write};
-use std::{env, fs, path};
+use clap::{Parser, Subcommand};
+use git_auth::{GitRequest, get_oauth, send_creds};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    // Get a credential
+    Get,
+    Store,
+    Erase,
+    Init,
+}
 
 fn main() {
-    eprintln!("Calling  git-auth");
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 2 || args[1] != "get" {
-        return;
-    }
-
-    let mut buffer = Vec::new();
-    let path_str = args[1].clone() + ".txt";
-    let path = path::Path::new(&path_str);
-    let mut stdout_file;
-    if path.exists() {
-        stdout_file = fs::File::options().append(true).open(path).unwrap();
-        let _ = stdout_file.write_all(b"\n\n\n");
-    } else {
-        stdout_file = fs::File::create(path).unwrap();
-    }
-    if !path::Path::new("pass.txt").exists() {
-        git_auth::get_oauth();
-    }
-
-    let res = io::stdin().read_to_end(&mut buffer);
-    match res {
-        Ok(_) => {
-            if !buffer.is_empty() {
-                let _ = stdout_file.write_all(&buffer);
-                buffer.clear();
-
-                git_auth::send_creds();
-            }
+    let cli = Cli::parse();
+    match cli.command {
+        Commands::Get => {
+            // let git_request = GitRequest::from_stdin();
+            // eprintln!("{:?}", git_request);
+            let creds = get_oauth();
+            send_creds(&creds);
         }
-        Err(_) => println!("FAILED"),
+        Commands::Init => eprintln!("Initialising"),
+        Commands::Store => eprintln!("Storeing"),
+        Commands::Erase => eprintln!("Eraseing"),
     }
 }
+
+// Old Code (Keep bits to use later)
+
+// use std::io::{self, Read, Write};
+// use std::{env, fs, path};
+// eprintln!("Calling  git-auth");
+// let args: Vec<String> = env::args().collect();
+// if args.len() != 2 || args[1] != "get" {
+//     return;
+// }
+//
+// let mut buffer = Vec::new();
+// let path_str = args[1].clone() + ".txt";
+// let path = path::Path::new(&path_str);
+// let mut stdout_file;
+// if path.exists() {
+//     stdout_file = fs::File::options().append(true).open(path).unwrap();
+//     let _ = stdout_file.write_all(b"\n\n\n");
+// } else {
+//     stdout_file = fs::File::create(path).unwrap();
+// }
+// if !path::Path::new("pass.txt").exists() {
+//     git_auth::get_oauth();
+// }
+//
+// let res = io::stdin().read_to_end(&mut buffer);
+// match res {
+//     Ok(_) => {
+//         if !buffer.is_empty() {
+//             let _ = stdout_file.write_all(&buffer);
+//             buffer.clear();
+//
+//             git_auth::send_creds();
+//         }
+//     }
+//     Err(_) => println!("FAILED"),
+// }
